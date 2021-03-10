718774d158758f8caacda31beee2ebcb33c85c56
b.adamiat <b.adamiat@irisaco.com>
2021-03-10 08:32:02 +0330

    pizza App

---
+++ src/components/Products/data.js
@@ -0,0 +1,60 @@
+import product1 from '../../images/pexels-pizza-2 (1).jpg';
+import product2 from '../../images/pexels-pizza-3.jpg';
+import product3 from '../../images/pexels-pizza-4.jpg';
+import Sweet1 from '../../images/Coffee.jpg';
+import Sweet2 from '../../images/Dessert-1.jpg';
+import Sweet3 from '../../images/Dessert-2.jpg';
+
+export const productData = [
+    {
+        img: product1,
+        alt:'Pizza',
+        name:'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roam tomatoes, olives, and pasto',
+        price: '$19.99',
+        button:'Add to Cart'
+    },
+    {
+        img: product2,
+        alt:'Pizza',
+        name:'Hawaiian Paradise',
+        desc: 'Marinara sauce, basil, italian sausage, roam tomatoes, olives, and pasto',
+        price: '$16.99',
+        button:'Add to Cart'
+    },
+    {
+        img: product3,
+        alt:'Pizza',
+        name:'Veggie Overload',
+        desc: 'Marinara sauce, basil, italian sausage, roam tomatoes, olives, and pasto',
+        price: '$14.99',
+        button:'Add to Cart'
+    }
+]
+
+export const productDataTwo = [
+    {
+        img: Sweet1,
+        alt:'Sweet',
+        name:'Coffee',
+        desc: 'Marinara sauce, basil, italian sausage, roam tomatoes, olives, and pasto',
+        price: '$19.99',
+        button:'Add to Cart'
+    },
+    {
+        img: Sweet2,
+        alt:'Sweet',
+        name:'Dessert',
+        desc: 'Marinara sauce, basil, italian sausage, roam tomatoes, olives, and pasto',
+        price: '$16.99',
+        button:'Add to Cart'
+    },
+    {
+        img: Sweet3,
+        alt:'Sweet',
+        name:'Chocolate',
+        desc: 'Marinara sauce, basil, italian sausage, roam tomatoes, olives, and pasto',
+        price: '$14.99',
+        button:'Add to Cart'
+    }
+]
